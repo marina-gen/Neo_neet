de9e2d49fde3b67dbf088e6e68ab1ccf972c905f
marina-gen <ishitomochi@gmail.com>
2021-01-15 10:34:37 +0900

    has_secure_passowrd追加、adminカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_010507) do
+ActiveRecord::Schema.define(version: 2021_01_15_013026) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_010507) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
