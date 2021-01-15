feb0703dfb00a62c8b7bda4bafa22e5a5f99e1d9
marina-gen <ishitomochi@gmail.com>
2021-01-15 11:36:08 +0900

    ユーザー登録画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/lodout', to: 'sessions#destroy'
+  namespace :admin do
+    resources :users
+  end
+  get 'localhost:3000/main_screen' => 'schedule#index'
+  resources :schedules
+  root to: 'main_screens#index'
+  resources :main_screens
 end
