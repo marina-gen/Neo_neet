a1590914910c8c877808355f2e1b935f51cd755d
marina-gen <ishitomochi@gmail.com>
2021-01-16 04:05:43 +0900

    main senni

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,16 @@
+class SchedulesController < ApplicationController
+    def index
+        
+    end
+    def create
+        schedule = Schedule.new(schedule_params)
+    
+        schedule.save!
+        redirect_to("")#リダイレクト先URL
+
+    end
+
+    def schedule_params
+        params.require(:schedule).permit(:name, :startdate, :memo)
+    end
+end
