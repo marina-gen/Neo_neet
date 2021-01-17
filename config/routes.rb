dca6cf90fff843f89e89dc2833aab8c8ed9a8710
Suzumy <ryouga.0085@gmail.com>
2021-01-17 17:47:46 +0900

    コミットし忘れたファイルをコミット

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,9 @@ Rails.application.routes.draw do
     resources :users
   end
 
-<<<<<<< HEAD
   root to: 'karimain#index'
-=======
   resources :pub_users, only: [:create, :new, :show]
   # root to: 'main_screens#index'
->>>>>>> 53514bd23ba8b696121fe8b85a9e5fb8a0bc63ab
   get '/signup',to: 'admin/users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root to: 'calendars#index'
