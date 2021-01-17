eb4361015054cd1f56a3ecc38d29d9518774f7f4
marina-gen <55265241+marina-gen@users.noreply.github.com>
2021-01-17 17:38:47 +0900

    Revert "ログインしてない状態の仮メイン画面へのアクセスを拒否"

--- test/controllers/kari_main_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class KariMainControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get kari_main_index_url
-    assert_response :success
-  end
-
-end
--- test/controllers/karimain_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class KarimainControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get karimain_index_url
-    assert_response :success
-  end
-
-end
