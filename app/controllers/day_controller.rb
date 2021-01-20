3b8406f69a5a0b41c75bccec655b9fb9e5cc973c
marina-gen <ishitomochi@gmail.com>
2021-01-20 21:00:26 +0900

    日付画面でそれぞれのデータを表示

--- app/controllers/day_controller.rb
+++ app/controllers/day_controller.rb
@@ -1,8 +1,19 @@
 class DayController < ApplicationController
     def index
-        @date = params[:param1]
-        @wday = params[:param2]
-        @sday = Date.today
+        @date = params[:param1] #　クリックされた日付の年月日データが格納されている
+        @wday = params[:param2] #  曜日の数値データ
+        @sday = Date.today      #　インターネットから取得する今日の日付情報
+        
+        @tschedules = Schedule.where(user_id: current_user, startdate: @date)
+        if @tschedules.first == nil
+            @noschedule = "今日のクエストはありません"
+        end
+
+        @tmain_diaries = MainDiary.where(user_id: current_user, today: @date)
+        if @tschedules.first == nil
+            @nosavedata = "冒険の書はまだかきこまれていません"
+        end
+
     end
 
 end
